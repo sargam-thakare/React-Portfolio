5908aff44f7e70164ed24f75bc49d911550264e1
sargam <sargamthakare@gmail.com>
2021-07-14 20:34:28 +0530

    Updates

---
+++ precache-manifest.08f51121bc95de0ea2b8c833de27ab82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9635c3edd8410f71609",
+    "url": "/React-Portfolio/static/css/main.2a2c52b4.chunk.css"
+  },
+  {
+    "revision": "c9635c3edd8410f71609",
+    "url": "/React-Portfolio/static/js/main.c9635c3e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "f3af909686138e5efc63f10eb5351422",
+    "url": "/React-Portfolio/index.html"
+  }
+];
